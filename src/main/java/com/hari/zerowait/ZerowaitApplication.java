6245a606092e798ab8b06e7982186c1726e756a9
haryprasadh <hariprasad81026@gamil.com>
2025-07-18 23:52:05 +0530

    first commit

---
+++ src/main/java/com/hari/zerowait/ZerowaitApplication.java
@@ -0,0 +1,13 @@
+package com.hari.zerowait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerowaitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerowaitApplication.class, args);
+	}
+
+}
